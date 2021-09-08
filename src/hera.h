dad24f4a60bc735fe89632d0652913506eee897a
Guy Gastineau <strings.stringsandstrings@gmail.com>
2021-09-07 22:51:09 -0400

    Passing initial tests can get most likely wireless ifname.

--- src/hera.h
+++ src/hera.h
@@ -4,10 +4,19 @@
 #include <linux/wireless.h>
 
 int iw_get_socket();
+
+// Commit pending changes to driver. Does this require root?
+int iw_commit(void);
+int iw_commit_(int sock);
+
+// Get wireless extensions name.
 int iw_get_we_string(int sock, char *ifname, char *dest, size_t dest_size);
-// Get devices as ifreqs written into your buffer.
-int iw_get_devices_(int sock, char *dest, size_t size);
+
 // Get device names written into your own char buffers of IFNAMSIZ.
 int iw_get_devices(int sock, char (*dest)[IFNAMSIZ], size_t length);
+// Get devices as ifreqs written into your buffer.
+int iw_get_devices_(int sock, char *dest, size_t size);
+
+
 
 #endif // HERA_H
--- test/test_hera.c
+++ test/test_hera.c
@@ -3,6 +3,8 @@
 
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
 
 void setUp(void)
 {
@@ -12,6 +14,36 @@ void tearDown(void)
 {
 }
 
+// Try to find a wireless interface.
+// We basically just search for the letter 'w' in the if names.
+// This is good enough for now, but we should probably do better.
+#define NAME_LENGTH 12
+static char *most_likely_wireless_nic(int sock)
+{
+  char *dest;
+  char names[NAME_LENGTH][IFNAMSIZ];
+  int count = iw_get_devices(sock, names, NAME_LENGTH);
+  for (int i = 0; i < count && i < NAME_LENGTH; ++i) {
+    // Check the assembly to make sure it optimizes the mem access to a register.
+    for (int j = 0; j < IFNAMSIZ && names[i][j]; ++j) {
+      if (names[i][j] == 'w') {
+        dest = malloc(IFNAMSIZ);
+        if (!dest) {
+          return NULL;
+        }
+        if (!memcpy(dest, names[i], IFNAMSIZ)) {
+          free(dest);
+          return NULL;
+        }
+        return dest;
+      }
+    }
+  }
+  return NULL;
+}
+#undef NAME_LENGTH
+
+// pen and test it (then leave it for )
 static void test_get_iw_socket(void)
 {
   int sock = iw_get_socket();
@@ -25,14 +57,6 @@ static void test_get_devices_lowlevel(void)
   struct ifreq ifrs[12];
   int count = iw_get_devices_(sock, (char *)ifrs, sizeof(ifrs));
   TEST_ASSERT_MESSAGE(count > 0, "Failed to get devices.");
-
-#ifdef DEBUG
-  printf("Got %d devices:\n", count);
-  for (int i = 0; i < count; ++i) {
-    printf("  %s\n", ifrs[i].ifr_name);
-  }
-#endif // DEBUG
-
   close(sock);
 }
 
@@ -42,16 +66,20 @@ static void test_get_devices_highlevel(void)
   char names[12][IFNAMSIZ];
   int count = iw_get_devices(sock, names, 12);
   TEST_ASSERT_MESSAGE(count > 0, "Failed to get devices.");
-
-#ifdef DEBUG
-  printf("Got %d devices:\n", count);
-  for (int i = 0; i < count; ++i) {
-    printf("  %s\n", names[i]);
-  }
-#endif // DEBUG
-
   close(sock);
 }
+
+// Internal test! Just make sure we can determine the name.
+static void test_get_wireless_device(void)
+{
+  int sock = iw_get_socket();
+  char * ifname = most_likely_wireless_nic(sock);
+  TEST_ASSERT_NOT_NULL_MESSAGE(ifname, "Failed to get wireless interface name!");
+  free(ifname);
+  close(sock);
+}
+
+
 
 int main(void)
 {
@@ -60,6 +88,7 @@ int main(void)
    RUN_TEST(test_get_iw_socket);
    RUN_TEST(test_get_devices_lowlevel);
    RUN_TEST(test_get_devices_highlevel);
+   RUN_TEST(test_get_wireless_device);
 
    return UnityEnd();
 }
